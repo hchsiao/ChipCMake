0bad98a1c95ae1a6b1355d3dec6483f518af0d52
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2018-11-06 16:46:05 +0800

    add comments to Find modules

--- FindCBDK.cmake
+++ FindCBDK.cmake
@@ -1,4 +1,6 @@
+# CBDK_DIR: the absolute path to the cell-base design kit
 set(CBDK_DIR               /opt/CIC/share/CBDK_TSMC90GUTM_Arm_f1.0)
+# CBDK_DB_PATH: path from the CBDK_DIR to where library files (e.g. slow.db) stored
 set(CBDK_DB_PATH           CIC/SynopsysDC/db)
 set(CBDK_TARGET_LIBRARY    typical.db)
 set(CBDK_SYMBOL_LIBRARY    tsmc090.sdb)
--- FindVCS.cmake
+++ FindVCS.cmake
@@ -1,5 +1,7 @@
 include(VCS)
 
+# VCS_EXECUTABLE: which starts the vcs program
 set(VCS_EXECUTABLE /opt/CIC/bin/vcs)
+# VCS_ENV_FILE: path to the script that let you do "source $VCS_ENV_FILE; vcs &"
 set(VCS_ENV_FILE /opt/CIC/snps/vcs.bash)
 set(RTL_SIMULATOR_FOUND 1)
